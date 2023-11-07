14e128c610486c1b5149f77fffd3161ca8a32191
sanket-ifinworth <sanket@ifinworth.com>
2023-11-07 17:51:21 +0530

    feature:added content edit in preview

--- src/app/shared/modal box/generic-modal-box/generic-modal-box.component.ts
+++ src/app/shared/modal box/generic-modal-box/generic-modal-box.component.ts
@@ -43,50 +43,6 @@ ngOnInit() {
     this.webViewer()
 }
 async webViewer(){
-  // if (this.viewerRef && this.viewerRef.nativeElement && this.showWebViewer) {
-  //   const instance = await WebViewer({
-  //     path: '../../../../assets/lib',
-  //     fullAPI:true,
-  //     licenseKey:environment.webViewerLicense, 
-  //   }, this.viewerRef.nativeElement);
-      
-  //   const blob = new Blob([this.data.dataBlob], { type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' });
-  //   const blobUrl = URL.createObjectURL(blob);
-  //   instance.UI.loadDocument(
-  //     blobUrl,
-  //     {
-  //       filename: 'report.docx',
-  //       enableOfficeEditing: true,
-  //     });
-
-  //     const { documentViewer, annotationManager } = instance.Core;
-
-  //     instance.UI.disableElements(['menuButton'])
-    
-  //     instance.UI.setHeaderItems(header => {
-  //       header.push({
-  //           type: 'actionButton',
-  //           // img: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>',
-  //           img:'../../../../assets/save-file-logo.gif',
-  //           onClick: async () => 
-  //           {
-  //             const doc = documentViewer.getDocument();
-  //             const xfdfString = await annotationManager.exportAnnotations();
-  //             const data = await doc.getFileData({
-  //               // saves the document with annotations in it
-  //               xfdfString
-  //             });
-  //             const arr = new Uint8Array(data);
-  //             const blob = new Blob([arr], { type: 'application/docx' });
-  //             const url = URL.createObjectURL(blob);
-  //             // window.open(url);
-  //           }
-  //       });
-  //     });
-    
-  // } else {
-  //   console.error('Viewer element not found or not initialized.');
-  // }
   this.htmlContent = this.data?.dataBlob;
 }
 
