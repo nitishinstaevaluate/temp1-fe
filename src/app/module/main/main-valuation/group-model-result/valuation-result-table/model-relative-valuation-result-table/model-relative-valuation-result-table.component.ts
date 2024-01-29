abb2febc1cdd1ec6d22339e99fa992b765e31fbb
sanket-ifinworth <sanket@ifinworth.com>
2024-01-29 18:34:44 +0530

    fix:model updated

--- src/app/module/main/main-valuation/group-model-result/valuation-result-table/model-relative-valuation-result-table/model-relative-valuation-result-table.component.ts
+++ src/app/module/main/main-valuation/group-model-result/valuation-result-table/model-relative-valuation-result-table/model-relative-valuation-result-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { formatNumber } from 'src/app/shared/enums/functions';
 
 @Component({
   selector: 'app-model-relative-valuation-result-table',
@@ -9,6 +10,7 @@ export class ModelRelativeValuationResultTableComponent implements OnChanges{
   @Input() dataSource:any;
   @Input() formData:any;
   tableResult:any='';
+  formatPositiveAndNegativeNumber=formatNumber;
   ngOnChanges(changes: SimpleChanges): void {
     this.dataSource;
     this.formData;
