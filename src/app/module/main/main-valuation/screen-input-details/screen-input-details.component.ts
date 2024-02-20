bd8965b3945a9e7eec533bd75ae02bcd571401e4
sanket-ifinworth <sanket@ifinworth.com>
2024-02-20 18:29:31 +0530

    fix:updated the checkbox condition

--- src/app/module/main/main-valuation/screen-input-details/screen-input-details.component.ts
+++ src/app/module/main/main-valuation/screen-input-details/screen-input-details.component.ts
@@ -379,6 +379,9 @@ export class ScreenInputDetailsComponent implements OnInit,OnChanges {
             row.isSelected = this.selectedIndustries.some((selectedIndustriesRow: any) => {
               return selectedIndustriesRow.COMPANYID === row.COMPANYID;
             });
+            if(this.mainIndustries.length && !this.selectAll){
+              this.selectAll = true;
+            }            
           });
         }
         this.total = industryData.total;
@@ -576,7 +579,8 @@ export class ScreenInputDetailsComponent implements OnInit,OnChanges {
       await this.removeUncheckedIndustry(data)
     }
     // event.stopPropagation();
-    this.selectAll = this.ciqIndustryData.every((row:any) => row.isSelected);
+    // this.selectAll = this.ciqIndustryData.every((row:any) => row.isSelected);
+    this.selectAll = true;
   }
 
   async createIndustryStructure(data:any){
