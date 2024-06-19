c121a13fcb54217e44cf72795fccbed076a87620
sanket-ifinworth <sanket@ifinworth.com>
2024-06-19 15:24:35 +0530

    fix:updated sebi preview conditions

--- src/app/module/main/main-valuation/report-details/report-details.component.ts
+++ src/app/module/main/main-valuation/report-details/report-details.component.ts
@@ -264,7 +264,7 @@ export class ReportDetailsComponent implements OnInit,AfterViewInit {
         case this.transferStepperFour?.formOneAndThreeData?.model.includes(MODELS.NAV) && this.transferStepperFour?.formOneAndThreeData?.model.length === 1:
             reportService = this.previewNavReport.bind(this);
             break;
-        case this.reportForm.controls['reportPurpose'].value.some((item:any)=> item?.value.includes('sebiRegulations')):
+        case this.reportForm.controls['reportPurpose'].value.some((item:any)=> item?.value.includes('sebiRegulations')) && this.reportForm.controls['reportPurpose'].value?.length === 1:
             reportService = this.previewSebiReport.bind(this);
             break;
         default:
